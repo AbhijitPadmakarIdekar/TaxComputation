f277d804636b397250039af693cbdb6c9f830639
Abhijit Idekar <abhijit.padmakar.idekar@gmail.com>
2023-09-14 19:13:36 +0530

    Added Half Implimented Views

--- TaxComputation.Domain/Entities/Company.cs
+++ TaxComputation.Domain/Entities/Company.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TaxComputation.Domain.Entities
 {
     public class Company
     {
+        [Key]
         public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Company Name.")]
+        [Display(Name = "Company Name")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please Enter City Name.")]
+        [Display(Name = "City")]
         public string CompanyCity { get; set; } = string.Empty;
-        public string GstNumber { get; set; } = string.Empty;
-        public string ContactNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please Enter GST Number.")]
+        [Display(Name = "GST Number")]
+        public int GstNumber { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Contact Number.")]
+        [Display(Name = "Conatct Number")]
+        public int ContactNumber { get; set; }
         public List<Product>? Products { get; set; }
     }
 }
--- TaxComputation.Ui/Controllers/HomeController.cs
+++ TaxComputation.Ui/Controllers/HomeController.cs
@@ -13,11 +13,19 @@ namespace TaxComputation.Ui.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult Privacy()
         {
             return View();
